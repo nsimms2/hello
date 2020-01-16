a3b1bcf22658cb5cf02c29390436aa26378be59c
cchavez4 <59975010+cchavez4@users.noreply.github.com>
2020-01-16 13:45:32 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -4,5 +4,6 @@ int main(void)
 {
 printf("Nathan Simms\n");
 printf("My majoor is mechanical engineering\n")
+printf("My name is Carlos Chavez and my major is Software Engineering\n");
 return 0;
 }
